5f9434df408e6aa9831df3b83217a7476920419a
Bink <bink@onlinq.nl>
2024-07-23 15:34:03 +0200

    Model, controller, migration and routing setup

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);
